ca6a2fd9627356e6817f5daf2d8117b5e0fcd866
Why Do We Wonder <37032797+WhyDoWeWonder@users.noreply.github.com>
2021-11-19 11:52:55 -0500

    Intermediary commit to save work, new, dumber approach to the config file

--- structs/config.py
+++ structs/config.py
@@ -1,4 +1,5 @@
 import json
+import pathlib
 from warnings import warn
 from pathlib import Path
 import os
@@ -6,7 +7,7 @@ import os
 from structs.global_vars import GlobalVariables
 
 
-def get_config_file(config_path_string: str):
+def get_config_file(config_path_string: str, config_file_name = "config.json"):
     config_location = Path(config_path_string).expanduser().resolve()
 
     try:
@@ -14,9 +15,9 @@ def get_config_file(config_path_string: str):
         # Or the full path, this will open it either way
         with open(config_location, mode="r") as config_file:
             config = json.loads(config_file.read())
-        # If it's a directory we'll assume the file called config.json is the config
+        # If it's a directory we'll try to load config_location/config_file_name as the config file
     except IsADirectoryError:
-        with open(os.path.join(config_location, "config.json"), mode="r") as config_file:
+        with open(os.path.join(config_location, config_file_name), mode="r") as config_file:
             config = json.loads(config_file.read())
     except Exception as e:
         warn("Error finding and loading config file, using default config")
@@ -25,12 +26,44 @@ def get_config_file(config_path_string: str):
 
 
 class Config:
-    def __init__(self, config_location):
+
+    def __find_config_folder__(self):
+        if self.__config_location__ is not None:
+            # Deal with argparse passing strings in as lists with 1 entry
+            if isinstance(self.__config_location__, list):
+                self.__config_location__ = self.__config_location__[0]
+        else:
+            self.__config_location__ = self.__default_config_name__
+
+        path = Path(self.__config_location__).expanduser().absolute()
+        if not path.exists():
+            raise FileNotFoundError("Could not find a file or folder at " + self.__config_location__)
+        if path.is_dir():
+            path = pathlib.Path.joinpath(path, self.__default_config_name__)
+            if path.is_file():
+                self.__load_config__(path)
+            else:
+                raise FileNotFoundError(
+                    "Could not find config file named " + self.__default_config_name__ + " in folder " +
+                    self.__config_location__)
+        elif path.is_file():
+            self.__load_config__(path)
+
+    def __load_config__(self, path):
+        with path.open(mode="r") as file:
+            self.config = json.loads(file.read())
+
+    def __init__(self, config_location, default_config_name="config.json"):
         # Argparse currently passes in arguments of certain types wrapped in a list when it shouldn't
         # This is workaround for that issue
-        if isinstance(config_location, list):
-            config_location = config_location[0]
-        self.config = get_config_file(config_location)
 
-    def get_config(self):
-        return self.config
+        # If config_location is a folder, this option specifies what file to look for in the folder as the config
+
+        if self.__config_location__ is not None:
+            # Deal with argparse passing strings in as lists with 1 entry
+            if isinstance(self.__config_location__, list):
+                self.__config_location__ = self.__config_location__[0]
+        else:
+            self.__config_location__ = self.__default_config_name__
+
+        self.config = get_config_file(config_location, default_config_name)
